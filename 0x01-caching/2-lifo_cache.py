02ac42954d3e90b7cd0f50c2819004b91df1bcb0
Broline <brolineoronu3@gmail.com>
2023-07-26 18:25:50 -0700

    LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """
-Basic dictionary FIFO Cache
+Basic dictionary LIFO Cache
 """
 
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """BaseCaching Caching System"""
+class LIFOCache(BaseCaching):
+    """LIFOCache Caching System"""
 
     def __init__(self) -> None:
         """Init function"""
         super().__init__()
+        self.last = ""
 
     def put(self, key, item):
         """Put function"""
         if key and item:
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                firstItem = list(self.cache_data.keys())[0]
-                print("DISCARD: {}".format(firstItem))
-                del(self.cache_data[firstItem])
+                print("DISCARD: {}".format(self.last))
+                del(self.cache_data[self.last])
+            self.last = key
 
     def get(self, key):
         """Get function"""
