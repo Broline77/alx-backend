bd930b0435afac7fb1be04fd0dd6cc8f11ccbb84
broline <brolineoronu3@gmail.com>
2023-08-13 17:50:40 +0300

    mock login

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Use user locale
+"""Force locale with URL parameter
 """
 
 
@@ -45,50 +45,28 @@ def get_user() -> dict:
 @app.before_request
 def before_request():
     """find a user if any, and set it as a global on flask.g.user"""
-    user = get_user()
-    g.user = user
+    if get_user():
+        g.user = get_user()
 
 
 @babel.localeselector
-def get_locale() -> str:
+def get_locale():
     """
-    Returns local
-    user_locale = None
-    if g.user:
-        user_locale = g.user.get('locale')
-    locale = request.args.get('locale') or user_locale
+    Returns locale
+    """
+    locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
+        print(locale)
         return locale
-    """
-    locale = request.args.get('locale', '')
-    if locale in app.config["LANGUAGES"]:
-        return locale
-    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
-        return g.user['locale']
-    header_locale = request.headers.get('locale', '')
-    if header_locale in app.config["LANGUAGES"]:
-        return header_locale
+
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@babel.timezoneselector
-def get_timezone() -> str:
-    """Retrieves the timezone for a web page.
-    """
-    timezone = request.args.get('timezone', '').strip()
-    if not timezone and g.user:
-        timezone = g.user['timezone']
-    try:
-        return pytz.timezone(timezone).zone
-    except pytz.exceptions.UnknownTimeZoneError:
-        return app.config['BABEL_DEFAULT_TIMEZONE']
 
 
 @app.route('/')
 def index():
     """mocking log in
     """
-    return render_template('6-index.html', users=users)
+    return render_template('5-index.html', users=users)
 
 
 if __name__ == '__main__':
